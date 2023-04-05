1a1cc792c32c2ed66569859046edd7b69ec24e64
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-04-05 13:35:14 +0200

    Update Taskbar

--- Assets/Scripts/TaskBarBackend.cs
+++ Assets/Scripts/TaskBarBackend.cs
@@ -1,17 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TaskBarBackend : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static TaskBarBackend Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+    }
+
+    public void OnRightButtonClick()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnLeftButtonClick()
     {
         
     }
