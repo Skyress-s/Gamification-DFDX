2d2bab25688265eca3316bc7d820486909ee0df5
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-04-05 14:28:48 +0200

    First try at navbar

--- Assets/Scripts/TaskBarBackend.cs
+++ Assets/Scripts/TaskBarBackend.cs
@@ -1,7 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TaskBarBackend : MonoBehaviour
 {
+
+    [SerializeField] private string noteSceneName;
+    [SerializeField] private string profileSceneName;
+
+    private string _currentSceneName;
+    private string _prevSceneName = "S_MainMenu";
+    private SceneHanlderComponent _sceneHanlderComp;
+
+    // private enum NavState
+    // {
+    //     NotesOpen,
+    //     ProfileOpen,
+    //     NeitherOpen
+    // }
+    //
+    // public enum ButtonLabel
+    // {
+    //     Left,
+    //     Right
+    // }
+
+    // private NavState _currentState;
+
     public static TaskBarBackend Instance { get; private set; }
 
     private void Awake()
@@ -17,13 +42,33 @@ public class TaskBarBackend : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _sceneHanlderComp = GetComponent<SceneHanlderComponent>();
+        _currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
     public void OnRightButtonClick()
     {
-        
+        _currentSceneName = SceneManager.GetActiveScene().name;
+        if (_currentSceneName == profileSceneName)
+        {
+            _sceneHanlderComp.LoadScene(_prevSceneName);
+            return;
+        }
+        _prevSceneName = _currentSceneName;
+        _sceneHanlderComp.LoadScene(profileSceneName);
     }
 
     public void OnLeftButtonClick()
     {
-        
+        _currentSceneName = SceneManager.GetActiveScene().name;
+        if (_currentSceneName == noteSceneName)
+        {
+            _sceneHanlderComp.LoadScene(_prevSceneName);
+            return;
+        }
+        _prevSceneName = _currentSceneName;
+        _sceneHanlderComp.LoadScene(noteSceneName);
     }
 }
