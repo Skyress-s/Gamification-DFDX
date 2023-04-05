6707885957fc801ebd50e45eef2ee4a9bceae6a9
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-04-05 15:48:19 +0200

    Added back button

--- Assets/Scripts/TaskBarBackend.cs
+++ Assets/Scripts/TaskBarBackend.cs
@@ -7,68 +7,66 @@ public class TaskBarBackend : MonoBehaviour
 
     [SerializeField] private string noteSceneName;
     [SerializeField] private string profileSceneName;
+    [SerializeField] private string menuSceneName;
 
     private string _currentSceneName;
     private string _prevSceneName = "S_MainMenu";
     private SceneHanlderComponent _sceneHanlderComp;
-
-    // private enum NavState
-    // {
-    //     NotesOpen,
-    //     ProfileOpen,
-    //     NeitherOpen
-    // }
-    //
-    // public enum ButtonLabel
-    // {
-    //     Left,
-    //     Right
-    // }
-
-    // private NavState _currentState;
-
-    public static TaskBarBackend Instance { get; private set; }
-
-    private void Awake()
-    {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(Instance);
-        }
-    }
+    private DataManager _dataManager;
 
     private void Start()
     {
+        _dataManager = FindObjectOfType<DataManager>();
         _sceneHanlderComp = GetComponent<SceneHanlderComponent>();
         _currentSceneName = SceneManager.GetActiveScene().name;
     }
 
     public void OnRightButtonClick()
     {
+        if (_dataManager.stringsDictionary.ContainsKey("prevScene")) _prevSceneName = _dataManager.stringsDictionary["prevScene"];
+        
         _currentSceneName = SceneManager.GetActiveScene().name;
+        // Debug.LogError("Scenes: " + _currentSceneName + " | " + noteSceneName + " | " + _prevSceneName);
         if (_currentSceneName == profileSceneName)
         {
+            // Debug.LogError("return from profile");
             _sceneHanlderComp.LoadScene(_prevSceneName);
             return;
         }
+        if (_currentSceneName == noteSceneName)
+        {
+            return;   
+        }
+        // Debug.LogError("open profile");
         _prevSceneName = _currentSceneName;
+        _dataManager.stringsDictionary["prevScene"] = _prevSceneName;
         _sceneHanlderComp.LoadScene(profileSceneName);
     }
 
     public void OnLeftButtonClick()
     {
+        if (_dataManager.stringsDictionary.ContainsKey("prevScene")) _prevSceneName = _dataManager.stringsDictionary["prevScene"];
+        
         _currentSceneName = SceneManager.GetActiveScene().name;
+        Debug.LogError("Scenes: " + _currentSceneName + " | " + noteSceneName + " | " + _prevSceneName);
         if (_currentSceneName == noteSceneName)
         {
+            // Debug.LogError("return from notes");
             _sceneHanlderComp.LoadScene(_prevSceneName);
             return;
         }
+        if (_currentSceneName == profileSceneName)
+        {
+            return;   
+        }
+        // Debug.LogError("open notes");
         _prevSceneName = _currentSceneName;
+        _dataManager.stringsDictionary["prevScene"] = _prevSceneName;
         _sceneHanlderComp.LoadScene(noteSceneName);
+    }
+
+    public void OnBackButtonClick()
+    {
+        _sceneHanlderComp.LoadScene(menuSceneName);
     }
 }
