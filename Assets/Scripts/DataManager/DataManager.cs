f26fe28fab5d63c17083f554d853fa6a12ceb283
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2023-04-05 15:47:38 +0200

    Added DataManager

---
+++ Assets/Scripts/DataManager/DataManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+
+    public Dictionary<string, string> stringsDictionary = new Dictionary<string, string>();
+
+    public static DataManager Instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
